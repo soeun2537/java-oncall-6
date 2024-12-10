de6402d86e6d786e80819dc286b6455496791ae3
soeun2537 <soeun2537@gmail.com>
2024-12-10 19:25:33 +0900

    refactor: InputView 메서드 라인을 위한 함수 추출

--- src/main/java/oncall/view/InputView.java
+++ src/main/java/oncall/view/InputView.java
@@ -13,15 +13,24 @@ public class InputView {
     public static StartDate readStartDate() {
         String input = input();
         List<String> separatedInput = Parser.separateBySeparator(input, INPUT_SEPARATOR.getContent());
-        String month = separatedInput.get(0);
-        String day = separatedInput.get(1);
+        int month = validateMonth(separatedInput.get(0));
+        String day = validateDay(separatedInput);
+        return StartDate.from(month, day);
+    }
+
+    private static int validateMonth(String month) {
         Validator.validateNotNull(month);
         Validator.validateNumeric(month);
         int parsedMonth = Parser.convertStringToInt(month);
         Validator.validateMonth(parsedMonth);
+        return parsedMonth;
+    }
+
+    private static String validateDay(List<String> separatedInput) {
+        String day = separatedInput.get(1);
         Validator.validateNotNull(day);
         Validator.validateDay(day);
-        return StartDate.from(parsedMonth, day);
+        return day;
     }
 
     public static List<String> readWeekdayWorkers() {
