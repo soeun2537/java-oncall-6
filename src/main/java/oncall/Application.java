f4d99b282ed14eba074abf8cad40ae33e62377bb
soeun2537 <soeun2537@gmail.com>
2024-12-10 19:15:11 +0900

    feat: Application에 의존성 주입 설정

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,7 +1,12 @@
 package oncall;
 
+import oncall.controller.OncallController;
+import oncall.service.OncallSerivce;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OncallSerivce oncallSerivce = new OncallSerivce();
+        OncallController oncallController = new OncallController(oncallSerivce);
+        oncallController.run();
     }
 }
